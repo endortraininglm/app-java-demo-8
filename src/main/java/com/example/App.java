7e4576638752808f2b4d898ba5eb6863a4beb5dd
endor-lmoreno <lmoreno@endor.ai>
2026-09-03 16:13:26 -0500

    r2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 /**
  * Sample Java application with vulnerable dependencies
  * This application is intended for demonstration purposes only
- * Simple text diff
+ * Simple text diff diff 1
  */
 // Lombok data class example
 @Data
